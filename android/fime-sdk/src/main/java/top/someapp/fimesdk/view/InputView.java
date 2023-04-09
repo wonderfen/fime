641afc51023f4400c4e41fb238bfb915aa949128
zwz <zelde126@126.com>
2023-04-10 00:56:25 +0800

    refactor(app,fime-sdk): 移除 Room、cpp

---
+++ android/fime-sdk/src/main/java/top/someapp/fimesdk/view/InputView.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2023 Fime project https://fime.site
+ * Initial author: zelde126@126.com
+ */
+
+package top.someapp.fimesdk.view;
+
+/**
+ * @author zwz
+ * Create on 2023-04-09
+ */
+public class InputView {
+}
