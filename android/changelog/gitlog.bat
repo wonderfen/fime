6a995f69792713dc98715c76a5cb43c331b7b05d
zwz <zelde126@126.com>
2023-04-24 23:32:54 +0800

    ci: 脚本优化

--- android/changelog/gitlog.bat
+++ android/changelog/gitlog.bat
@@ -1,3 +1,4 @@
 @echo off
 chcp 65001
-git log --oneline --after="1 week ago" --pretty="format:- %%s" > %~dp0log.txt
+git log --oneline --after="1 week ago" --pretty="format:- %%s" ^
+--grep="^\(fix\|perf\|feat\|refactor\)\.*" > %~dp0log.txt
--- android/changelog/gitlog.sh
+++ android/changelog/gitlog.sh
@@ -1,1 +1,5 @@
-git log --oneline --after="1 week ago" --pretty="format:- %s" > $GITHUB_WORKSPACE/android/changelog/log.txt
+#!/usr/bin/env bash
+ABS_PATH=$(readlink -f "$0")
+ABS_PATH=$(dirname $ABS_PATH)
+git log --oneline --after="1 week ago" --pretty="format:- %s" \
+--grep="^\(fix\|perf\|feat\|refactor\)\.*" > $ABS_PATH/log.txt
