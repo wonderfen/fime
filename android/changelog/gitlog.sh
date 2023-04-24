cbde69a185856549bb9b5e3e4a77a5f037e0ec04
zwz <zelde126@126.com>
2023-04-25 00:12:58 +0800

    ci: 脚本优化 2

--- android/changelog/gitlog.sh
+++ android/changelog/gitlog.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ABS_PATH=$(readlink -f "$0")
-ABS_PATH=$(dirname $ABS_PATH)
+#ABS_PATH=$(readlink -f "$0")
+#ABS_PATH=$(dirname $ABS_PATH)
 git log --oneline --after="1 week ago" --pretty="format:- %s" \
---grep="^\(fix\|perf\|feat\|refactor\)\.*" > $ABS_PATH/log.txt
+--grep="^\(fix\|perf\|feat\|refactor\)\.*" > $GITHUB_WORKSPACE/android/changelog/log.txt
